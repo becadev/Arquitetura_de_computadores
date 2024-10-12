bae8a4dc6878dce12008488a1affa8449a6ae637
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-12 11:54:14 -0300

    Create dobro-de-n.s

---
+++ Questões/dobro-de-n.s
@@ -0,0 +1,10 @@
+.text
+main: addi $2, $0, 5 #	ler inteiro
+      syscall 
+      add $15, $0, $2 # adiciona o 2 ao $15
+      mult $15, $2
+      mflo $4 # coloca o resultado da multiplicação no $4
+      addi $2, $0, 1 # realiza a impressão
+      syscall 
+      addi $2, $0, 10 # finaliza o programa
+      syscall
