af2bd44ef638f2c8caaf9ceebd74a715209d2bf2
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-12 11:57:06 -0300

    Update dobro-de-n.asm

--- Questões/dobro-de-n.asm
+++ Questões/dobro-de-n.asm
@@ -1,10 +1,10 @@
 .text
-main: addi $2, $0, 5 #ler inteiro
+main: addi $2, $0, 5 # ler inteiro
       syscall 
-      add $15, $0, $2 #adiciona o 2 ao $15
+      add $15, $0, $2 # adiciona o 2 ao $15
       mult $15, $2
-      mflo $4 #coloca o resultado da multiplicação no $4
-      addi $2, $0, 1 #realiza a impressão
+      mflo $4 # coloca o resultado da multiplicação no $4
+      addi $2, $0, 1 # realiza a impressão
       syscall 
-      addi $2, $0, 10 #finaliza o programa
+      addi $2, $0, 10 # finaliza o programa
       syscall
