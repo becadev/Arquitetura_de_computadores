035b520fac9d6f87f721138f4c3a4c88c6223130
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-11-01 10:20:55 -0300

    Create maior_menor.asm

---
+++ Questões/SLT/maior_menor.asm
@@ -0,0 +1,21 @@
+.text
+main:	addi $2 $0 5
+	syscall
+	add $8 $0 $2
+	addi $2 $0 5
+	
+	syscall
+	add $9 $0 $2
+	
+	slt $10 $8 $9 # Se ($8 < $9) $10 <- 1 senao $10 <- 0
+	beq $10 $0 imp9 # Se for 0 imprimira o $9
+	add $4 $0 $8
+	j imp
+	
+imp9:	add $4 $0 $9
+
+imp: 	addi $2 $0 1
+	syscall
+	
+	addi $2 $0 10
+	syscall
