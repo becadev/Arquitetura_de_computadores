cc62a2b4844ad4c2e1d0e6f626000b0cc6064129
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-12-02 16:12:56 -0300

    Update Aula1.asm

--- Jogo/Anotacoes/Aula1.asm
+++ Jogo/Anotacoes/Aula1.asm
@@ -1,29 +1,35 @@
 .text
 main:
 	lui $8 0x1001
-	lui $9 0x00ff
+	ori $9 0x229a00
+	lui $10 0x1001
 	addi $20 $0 32
 	
-test: 	beq $20 $0 lateral
+test: 	beq $20 $0 lados
 	sw $9 0($8) 
-	addi $8 $8 4
+	
+	for:
+		sw $9 1920($8) 
+		addi $8 $8 4
+	
 	addi $20 $20 -1
 	j test
+lados:	
+	addi $20 $0 32
+	
+	
+test2:	beq $20 $0 fim
 
+	sw $9 0($10) 
+	addi $10 $8 124	# $8 está com o pc da linha anterior
 
-lateral:
 	sw $9 0($8) 
-	addi $8 $8 4
-	addi $
-test1:	
-	beq $
-	addi $8 $8 4
-	sw $9 120($8) 
-	addi $8 $8 4
+	addi $8 $8 128
+	
 	addi $20 $20 -1
-	j test1
+	j test2
 
 
-fimlados:
-	addi $2 $0 10
+
+fim:	addi $2 $0 10
 	syscall
